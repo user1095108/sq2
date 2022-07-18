14fc5603e8823da940101251be57786bcb439552
Janez Žemva <janezz55@gmail.com>
2022-07-18 20:14:11 +0200

    some fixes

---
+++ sq2.cpp
@@ -0,0 +1,30 @@
+#include "sq2.hpp"
+
+using namespace sq2::literals;
+
+//////////////////////////////////////////////////////////////////////////////
+int main()
+{
+  auto const db("example.db"_sq2.open_unique(
+      SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE
+    )
+  );
+
+  "DROP TABLE IF EXISTS COMPANY;"
+  "CREATE TABLE COMPANY("
+  "ID INT PRIMARY KEY     NOT NULL,"
+  "NAME           TEXT    NOT NULL,"
+  "AGE            INT     NOT NULL,"
+  "ADDRESS        CHAR(50),"
+  "SALARY         REAL);"
+  "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)"
+  "VALUES(1, 'Paul', 32, 'California', 20000.00);"
+  "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)"
+  "VALUES(2, 'Allen', 25, 'Texas', 15000.00 );"
+  "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)"
+  "VALUES(3, 'Teddy', 23, 'Norway', 20000.00 );"
+  "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)"
+  "VALUES(4, 'Mark', 25, 'Rich-Mond ', 65000.00)"_sq2.execmulti(db);
+
+  return 0;
+}
---
+++ sq2.hpp
@@ -0,0 +1,119 @@
+#ifndef SQ2_HPP
+# define SQ2_HPP
+# pragma once
+
+#include <cassert>
+#include <memory>
+#include <string_view>
+#include <type_traits>
+#include <utility>
+
+#include "sqlite3.h"
+
+namespace sq2
+{
+
+namespace detail
+{
+
+struct sqlite3_db_deleter
+{
+  void operator()(sqlite3* const p) const noexcept
+  {
+    sqlite3_close_v2(p);
+  }
+};
+
+struct sqlite3_stmt_deleter
+{
+  void operator()(sqlite3_stmt* const p) const noexcept
+  {
+    sqlite3_finalize(p);
+  }
+};
+
+}
+
+using shared_db_t = std::shared_ptr<sqlite3>;
+
+using unique_db_t = std::unique_ptr<
+  sqlite3,
+  detail::sqlite3_db_deleter
+>;
+
+using shared_stmt_t = std::shared_ptr<sqlite3_stmt>;
+
+using unique_stmt_t = std::unique_ptr<
+  sqlite3_stmt,
+  detail::sqlite3_stmt_deleter
+>;
+
+namespace detail
+{
+
+struct maker
+{
+  std::string_view const s_;
+
+  auto execmulti(auto&& db) && noexcept
+  {
+    return sqlite3_exec(db.get(), s_.data(), nullptr, nullptr, nullptr);
+  }
+
+  auto open_shared(int const fl = {}, char const* const zvfs = {}) && noexcept
+  {
+    sqlite3* db;
+
+    return SQLITE_OK == sqlite3_open_v2(s_.data(), &db, fl, zvfs) ?
+      shared_db_t(db, detail::sqlite3_db_deleter()) :
+      (detail::sqlite3_db_deleter()(db), shared_db_t());
+  }
+
+  auto open_unique(int const fl = {}, char const* const zvfs = {}) && noexcept
+  {
+    sqlite3* db;
+
+    return SQLITE_OK == sqlite3_open_v2(s_.data(), &db, fl, zvfs) ?
+      unique_db_t(db) :
+      (detail::sqlite3_db_deleter()(db), unique_db_t());
+  }
+
+  auto shared(sqlite3* const db, unsigned fl = {}) && noexcept
+  {
+    sqlite3_stmt* s;
+
+    auto const r(sqlite3_prepare_v3(db, s_.data(), s_.size(), fl, &s, {}));
+    assert(SQLITE_OK == r);
+
+    return SQLITE_OK == r ?
+      shared_stmt_t(s, detail::sqlite3_stmt_deleter()) :
+      shared_stmt_t();
+  }
+
+  auto unique(sqlite3* const db, unsigned fl = {}) && noexcept
+  {
+    sqlite3_stmt* s;
+
+    auto const r(sqlite3_prepare_v3(db, s_.data(), s_.size(), fl, &s, {}));
+    assert(SQLITE_OK == r);
+
+    return SQLITE_OK == r ? unique_stmt_t(s) : unique_stmt_t();
+  }
+};
+
+}
+
+namespace literals
+{
+
+inline auto operator "" _sq2(char const* const s, std::size_t const N)
+  noexcept
+{
+  return detail::maker{{s, N}};
+}
+
+}
+
+}
+
+#endif // SQ2_HPP
