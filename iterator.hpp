919ab8402276a944c83201f250cd0103c2549bec
Janez Žemva <janezz55@gmail.com>
2022-08-01 12:23:13 +0200

    some fixes

--- iterator.hpp
+++ iterator.hpp
@@ -30,7 +30,6 @@ public:
       std::tuple<A...>,
       std::tuple_element_t<0, std::tuple<A...>>
     >;
-  using pointer = value_type const*;
   using reference = value_type const&;
 
   sqlite3_stmt* s_;
