09656caf313a655cc9d22b0909d5b018a4f45d84
Janez Žemva <janezz55@gmail.com>
2022-08-05 14:35:23 +0200

    some fixes

--- iterator.hpp
+++ iterator.hpp
@@ -19,8 +19,7 @@ namespace sq2
 
 template <typename> struct tag{};
 
-template <int, typename T>
-void deserialize(sqlite3_stmt*, tag<T>) = delete;
+template <int> void deserialize() = delete;
 
 template <int I, typename ...A>
 class iterator
