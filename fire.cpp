b29bcdbcc5f34ce59753ed61965d0711915c0bb9
Janez Žemva <janezz55@gmail.com>
2025-07-03 12:02:09 +0200

    some fixes

--- fire.cpp
+++ fire.cpp
@@ -117,12 +117,11 @@ int main()
   {
     auto const frame_start(std::chrono::steady_clock::now());
 
-    sq2::step(replace_bottom, propagate);
+    // resets are not strictly necessary,
+    // https://stackoverflow.com/questions/35741175/sqlite3-reset-when-is-it-needed
+    sq2::step(replace_bottom, propagate, render);
 
     std::cout << *i << reset_screen;
-
-    i.reset();
-    //sq2::reset(replace_bottom, propagate);
 
     std::this_thread::sleep_until(frame_start + 33ms);
   }
